083506ec418a185c7d252316d3eeff72a3578924
Leandro Martins Guimarães <leandropl@gmail.com>
2023-09-10 21:20:07 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Notas de Negociação",
+    version="1.0",
+    description='Conversão de notas de corretagem em formato PDF para para uma estrutura de objetos Python',
+    author='Leandro Martins Guimarães',
+    url='https://github.com/leandroguimaraes/notas-negociacao',
+    packages=find_packages(),
+)
