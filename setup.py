d225c9c8e5bbc9ec9001d4c35acaf4a379618dd2
Leandro Martins Guimarães <leandropl@gmail.com>
2024-04-27 19:29:35 +0000

    Correção de setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="notasnegociacao",
+    version="1.0",
     description='Conversão de notas de corretagem em formato PDF para para uma estrutura de objetos Python',
     author='Leandro Martins Guimarães',
     url='https://github.com/leandroguimaraes/notas-negociacao',
